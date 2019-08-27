d5b230a21e07194367000c7e2cc4809aea0bbdaf
Max Yendall <yendallmax@gmail.com>
2019-08-27 14:51:13 +1000

    Account Type

---
+++ src/Models/AccountType.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace PestRegister\LaravelAccountingSync\Models;
+
+
+use PestRegister\LaravelAccountingSync\Interfaces\CrudInterface;
+
+class AccountType extends BaseModel implements CrudInterface
+{
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+
+    public function get(array $parameters = [])
+    {
+        $response = $this->getGateway()->getAccountType($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getAccountTypes();
+    }
+
+    public function create(array $parameters)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(array $parameters)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(array $parameters)
+    {
+        // TODO: Implement delete() method.
+    }
+}
