7f588f2d2dabb5e539bea77617d667671c1c51e8
Max Yendall <yendallmax@gmail.com>
2019-09-09 14:32:36 +1000

    Manual Journals

---
+++ src/Models/ManualJournal.php
@@ -0,0 +1,65 @@
+<?php
+namespace PestRegister\LaravelAccountingSync\Models;
+use PestRegister\LaravelAccountingSync\Interfaces\CrudInterface;
+
+class ManualJournal extends BaseModel implements CrudInterface
+{
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+    public function create(array $parameters = [])
+    {
+        $response = $this->getGateway()->createManualJournal($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getManualJournals();
+    }
+
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+
+    public function update(array $parameters = [])
+    {
+        $response = $this->getGateway()->updateManualJournals($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getManualJournals();
+    }
+
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+
+    public function get(array $parameters = [])
+    {
+        $response = $this->getGateway()->getManualJournal($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getManualJournals();
+    }
+
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+
+    public function delete(array $parameters = [])
+    {
+        $response = $this->getGateway()->deleteManualJournal($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getManualJournals();
+    }
+}
