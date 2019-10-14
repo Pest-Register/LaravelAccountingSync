6b5e92debf2019bbace54a15bd2321789ce119fe
Max Yendall <yendallmax@gmail.com>
2019-10-14 15:44:21 +1100

    Tax Rate Value addition

---
+++ src/Models/TaxRateValue.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: MaxYendall
+ * Date: 14/10/2019
+ * Time: 3:43 PM
+ */
+namespace PestRegister\LaravelAccountingSync\Models;
+
+
+use PestRegister\LaravelAccountingSync\Interfaces\CrudInterface;
+
+class TaxRateValue extends BaseModel implements CrudInterface
+{
+
+    public function create(array $parameters)
+    {
+        $response = $this->getGateway()->createTaxRateValue($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getTaxRates();
+    }
+
+    public function update(array $parameters)
+    {
+        $response = $this->getGateway()->updateTaxRateValue($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getTaxRates();
+    }
+
+    public function get(array $parameters)
+    {
+        $response = $this->getGateway()->getTaxRateValue($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getTaxRates();
+    }
+
+    public function delete(array $parameters)
+    {
+        $response = $this->getGateway()->deleteTaxRateValue($parameters)->send();
+        if (!$response->isSuccessful()) {
+            throw new \Exception($response->getErrorMessage());
+        }
+        return $response->getTaxRates();
+    }
+}
