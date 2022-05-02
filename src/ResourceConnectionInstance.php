711dc61c63ac4380f74286da3f93fce0275e51cb
Nick Zuccarelli <nicholaszuccarelli@gmail.com>
2022-05-02 16:06:15 +1000

    Search method for filtering provider results

--- src/ResourceConnectionInstance.php
+++ src/ResourceConnectionInstance.php
@@ -32,6 +32,27 @@ class ResourceConnectionInstance
     }
 
     /**
+     * Perform a query search to the provider
+     *
+     * @param $params: Normal params for endpoint request
+     * @param array $searchParams : Search parameters (key value pairs - must match keys/values supported by the provider)
+     * @param array $searchFilters : Search filters (key value pairs - must match keys/values supported by the provider)
+     * @param bool $exactSearchValue : Either perform an exact or partial match on the values supplied within $params
+     * @param bool $matchAllFilters : Either match all or at least one of the values supplied in $filters
+     * @return mixed
+     */
+    public function search (array $params = [], array $searchParams = [], array $searchFilters = [], bool $exactSearchValue = true, bool $matchAllFilters = false) {
+        $arr = $params + [
+            'search_params' => $searchParams,
+            'exact_search_value' => $exactSearchValue,
+            'search_filters' => $searchFilters,
+            'match_all_filters' => $matchAllFilters,
+        ];
+
+        return $this->accountingResourceInstance->get($arr);
+    }
+
+    /**
      * Magic method to call method of accountingResourceInstance
      * if the method is not defined on this Connector
      *
